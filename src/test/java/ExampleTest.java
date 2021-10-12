b0ff1c77f600f1cfa69d67780399bef566324f37
in7hesky <anton.mayko.qa@gmail.com>
2021-10-12 15:57:43 +0300

    save

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,8 +1,15 @@
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.*;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+@Owner("in7hesky")
 public class ExampleTest extends A_BaseTest
 {
     @Test
+    @Feature("login")
+    @Description("trying to describe a test")
+    @Severity(SeverityLevel.CRITICAL)
     public void loginViaEmail() {
         app.loginPage.open();
         app.loginPage.login("tomsmith", "SuperSecretPassword!");
